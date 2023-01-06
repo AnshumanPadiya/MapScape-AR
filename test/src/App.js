9f80a6297e9e50a684a2d54150727aa5729e6b1e
Anshuman Padiya <53999212+AnshumanPadiya@users.noreply.github.com>
2023-01-06 17:54:27 +0530

    done finally

--- test/src/App.js
+++ test/src/App.js
@@ -1,22 +1,28 @@
+import { useState } from "react";
 import CameraTesseract from "tesseract-with-html5-camera/dist/lib/index.js";
 import { dataSet } from './data/index';
+import { activateAR } from "@leoncvlt/ar-button";
 
 // const arr = ['Taj Mahal'];
 
 function App() {
   // console.log(dataSet);
+
+  const [render, setRender] = useState(false);
+
   const handleTakePhoto = (text) => {
     // Do stuff with recognized text...
     console.log("Recognize text: ", text.trim());
     console.log(dataSet[0].name)
     
-    if(text.trim() === dataSet[0].name) {
-      console.log("Recognize text: ", text);
-      console.log(dataSet[0].name);
-    } else {
-      console.log("not same");
-    }
-
+    // if(text.trim() === dataSet[0].name) {
+    //   console.log("Recognize text: ", text);
+    //   console.log(dataSet[0].name);
+    //   setRender(true);
+    // } else {
+    //   console.log("not same");
+    // }
+setRender(true);
     // for(let i = 0; dataSet.length; i++) {
     //   console.log(dataSet[i].name.trime);
     //   if (text.trime === dataSet[i].name.trime) {
@@ -28,6 +34,14 @@ function App() {
 
   };
 
+  const activate = () => {
+    // trigger on an user interaction, like a click
+    activateAR({
+      src: "https://github.com/AnshumanPadiya/MapScape-AR/blob/saransh/charminar_hyderabad.glb?raw=true",
+      title: "Char Minar Hyderabad",
+    });
+  };
+
   return (
     <>
       <h3>Check result in console</h3>
@@ -36,6 +50,10 @@ function App() {
           handleTakePhoto(text);
         }}
       />
+
+      <div style={{display: "flex", width: "100%", justifyContent: "center", position: "relative", top: "20px"}}>
+        {render ? <button onClick={activate}>Checkout in AR</button> : <></>}
+      </div>
     </>
   );
 }
--- test/src/data/index.js
+++ test/src/data/index.js
@@ -1,6 +1,6 @@
 export const dataSet = [
     {
-        name: 'Taj Mahal',
+        name: 'Char Minar',
         src: "",
         info: ""
     },
