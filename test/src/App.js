23f2ed6a86171b53a74fd7b15cc1ddf4b1e85d77
Aaliya Ali <aaliya01ali@gmail.com>
2023-01-06 17:21:39 +0530

    Add ar-button that renders Taj Mahal

--- test/src/App.js
+++ test/src/App.js
@@ -1,5 +1,8 @@
 import CameraTesseract from "tesseract-with-html5-camera/dist/lib/index.js";
-import { dataSet } from './data/index';
+import { dataSet } from "./data/index";
+import "@leoncvlt/ar-button";
+import "@leoncvlt/ar-button/styles.css";
+import { activateAR } from "@leoncvlt/ar-button";
 
 // const arr = ['Taj Mahal'];
 
@@ -8,9 +11,9 @@ function App() {
   const handleTakePhoto = (text) => {
     // Do stuff with recognized text...
     console.log("Recognize text: ", text.trim());
-    console.log(dataSet[0].name)
-    
-    if(text.trim() === dataSet[0].name) {
+    console.log(dataSet[0].name);
+
+    if (text.trim() === dataSet[0].name) {
       console.log("Recognize text: ", text);
       console.log(dataSet[0].name);
     } else {
@@ -23,19 +26,36 @@ function App() {
     //     console.log("Recognize text: ", text);
     //   } else {
     //     console.log("not same");
-    //   }      
+    //   }
     // }
-
+  };
+  const activate = () => {
+    // trigger on an user interaction, like a click
+    activateAR({
+      src: "https://github.com/AnshumanPadiya/MapScape-AR/blob/cameratesseract/test/src/data/new/ImageToStl.com_tajmahal.glb?raw=true",
+      link: "https://www.nasa.gov/",
+      title: "A 3D model of an astronaut",
+    });
   };
 
   return (
     <>
       <h3>Check result in console</h3>
-      <CameraTesseract
+      {/* <CameraTesseract
         onTextRecognize={(text) => {
           handleTakePhoto(text);
-        }}
-      />
+        }} 
+
+      />*/}
+      {/* <ar-button
+        src="https://github.com/leoncvlt/ar-button/raw/master/assets/Astronaut.glb"
+        ios-src="https://github.com/leoncvlt/ar-button/raw/master/assets/Astronaut.usdz"
+        link="https://www.nasa.gov/"
+        title="A 3D model of an astronaut"
+      >
+        See in Augmented Reality
+      </ar-button> */}
+      <button onClick={activate}>Checkout in AR</button>
     </>
   );
 }
